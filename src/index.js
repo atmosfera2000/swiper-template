c2f2b678302c0a911621dd27930f8abaf0ebcc97
atmosfera2000 <atmosfera2000@mail.ru>
2024-12-11 14:55:40 +0300

    creative effect start

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
+import { Pagination, EffectCreative } from 'swiper/modules';
 import 'swiper/scss';
-import 'swiper/scss/pagination'
+import 'swiper/scss/pagination';
+import 'swiper/scss/effect-creative';
 import './style.scss';
 
-const swiper = new Swiper ('.swiper', {  
-    //loop: true,
-    slidesPerView: 1.5,   
+const swiper = new Swiper ('.swiper', {
+    loop: true,
+    centeredSlides: true, 
     grabCursor: true,
-    //centeredSlides: true,    
-    //loopAdditionalSlides: 1, 
-    modules: [Pagination],     
+    modules: [Pagination, EffectCreative],     
     pagination: {
         el: '.swiper-pagination',
         dynamicBullets: true,
         dynamicMainBullets: 3,
     },
-    breakpoints: {
-        480: {
-            slidesPerView: 2,
+    slidesPerView: 3,
+    effect: 'creative',
+    creativeEffect: {
+        limitProgress: 2,
+        prev: {
+            translate: ["-100%", 0, 0],
+            opacity: 0.3,
+            scale: .9
         },
-        640: {
-            slidesPerView: 3,
-        },
-        1024: {
-            slidesPerView: 4,
-        },
-    }
+        next: {
+            translate: ["100%", 0, 0],
+            opacity: 0.3,
+            scale: .9
+        }
+    },
 })
 
 
